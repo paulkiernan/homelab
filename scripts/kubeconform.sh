c808e39b0cbf6ed35fe93f35b3b28dfa14269c6d
Paul Kiernan <paul@phaselab.co>
2026-04-30 10:21:47 -0700

    fix(ci): workflow checks + taskfiles

--- scripts/kubeconform.sh
+++ scripts/kubeconform.sh
@@ -6,8 +6,6 @@ KUBERNETES_DIR=$1
 
 [[ -z "${KUBERNETES_DIR}" ]] && echo "Kubernetes location not specified" && exit 1
 
-kustomize_args=("--load-restrictor=LoadRestrictionsNone")
-kustomize_config="kustomization.yaml"
 kubeconform_args=(
     "-strict"
     "-ignore-missing-schemas"
@@ -20,8 +18,8 @@ kubeconform_args=(
     "-verbose"
 )
 
-echo "=== Validating standalone manifests in ${KUBERNETES_DIR}/flux ==="
-find "${KUBERNETES_DIR}/flux" -maxdepth 1 -type f -name '*.yaml' -print0 | while IFS= read -r -d $'\0' file;
+echo "=== Validating manifests in ${KUBERNETES_DIR}/bootstrap/argocd ==="
+find "${KUBERNETES_DIR}/bootstrap/argocd" -type f -name '*.yaml' -not -name '*.sops.*' -print0 | while IFS= read -r -d $'\0' file;
   do
     kubeconform "${kubeconform_args[@]}" "${file}"
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
@@ -29,23 +27,10 @@ find "${KUBERNETES_DIR}/flux" -maxdepth 1 -type f -name '*.yaml' -print0 | while
     fi
 done
 
-echo "=== Validating kustomizations in ${KUBERNETES_DIR}/flux ==="
-find "${KUBERNETES_DIR}/flux" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
+echo "=== Validating manifests in ${KUBERNETES_DIR}/argocd/apps ==="
+find "${KUBERNETES_DIR}/argocd/apps" -type f -name '*.yaml' -not -name '*.sops.*' -print0 | while IFS= read -r -d $'\0' file;
   do
-    echo "=== Validating kustomizations in ${file/%$kustomize_config} ==="
-    kustomize build "${file/%$kustomize_config}" "${kustomize_args[@]}" | \
-      kubeconform "${kubeconform_args[@]}"
-    if [[ ${PIPESTATUS[0]} != 0 ]]; then
-      exit 1
-    fi
-done
-
-echo "=== Validating kustomizations in ${KUBERNETES_DIR}/apps ==="
-find "${KUBERNETES_DIR}/apps" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
-  do
-    echo "=== Validating kustomizations in ${file/%$kustomize_config} ==="
-    kustomize build "${file/%$kustomize_config}" "${kustomize_args[@]}" | \
-      kubeconform "${kubeconform_args[@]}"
+    kubeconform "${kubeconform_args[@]}" "${file}"
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
       exit 1
     fi
